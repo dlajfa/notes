0e4bcb9f9bcf0a9333ddf7ba0ebff53e2bed786c
Lajfa <dlajfa@hotmail.com>
2024-03-14 12:22:01 +0800

    Update homepage

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let notes = [
 ];
 
 app.get("/", (request, response) => {
-  response.send(`<h1>Hello Node</h1>`);
+  response.send(`<h1>Notes</h1>`);
 });
 
 app.get("/api/notes", (request, response) => {
